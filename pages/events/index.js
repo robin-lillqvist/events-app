302238adf4872d133559fde5acaee713ce5a69a5
Robin Lillqvist <robin.lillqvist@knowit.se>
2023-09-07 08:11:18 +0200

    prefetching data in all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,16 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 import { getAllEvents } from "@/helpers/api-utils";
 import EventsSearch from "./events-search";
 import EventList from "@/components/events/event-list";
 
-function Events() {
+function Events(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
+
+  console.log(events);
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -16,10 +19,28 @@ function Events() {
 
   return (
     <Fragment>
+      <Head>
+        <title>All my events</title>
+      </Head>
+      <Head>
+        <title>All Events</title>
+        <meta name="description" content="Find a lot of great events that allow you to evolve..." />
+      </Head>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList events={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default Events;
