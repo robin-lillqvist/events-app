188ea76567d8172c4dbca6cdf765680377cce975
Robin Lillqvist <robin.lillqvist@knowit.se>
2023-09-07 07:59:51 +0200

    fix error

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,7 @@
-import { getEventById, getAllEvents } from "@/helpers/api-utils.js";
+import { getEventById, getFeaturedEvents } from "@/helpers/api-utils.js";
 import EventSummary from "@/components/event-detail/event-summary";
 import EventContent from "@/components/event-detail/event-content";
 import EventLogistics from "@/components/event-detail/event-logistics";
-import ErrorAlert from "@/components/ui/error-alert.js";
-import Button from "../../components/ui/button";
 
 import { Fragment } from "react";
 
@@ -11,47 +9,27 @@ function SpecificEventPage(props) {
   const event = props.selectedEvent;
 
   if (!event) {
-    console.log("Didn't find ID");
     return (
-      <Fragment>
-        <div className="center">
-          <ErrorAlert>
-            <p>No event found!</p>
-          </ErrorAlert>
-          <Button link="/events">All events</Button>
-        </div>
-      </Fragment>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
   return (
     <Fragment>
-      <EventSummary
-        id={eventData.id}
-        title={eventData.title}
-        location={eventData.location}
-        date={eventData.date}
-        image={eventData.image}
-        key={eventData.id}
-      />
-      <EventLogistics
-        date={eventData.date}
-        address={eventData.location}
-        image={eventData.image}
-        imageAlt={eventData.title}
-      />
+      <EventSummary title={event.title} />
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
       <EventContent>
-        <p>{eventData.description}</p>
+        <p>{event.description}</p>
       </EventContent>
     </Fragment>
   );
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
-
-  console.log("Inuti getStaticPaths");
 
   return {
     paths: paths,
@@ -63,14 +41,16 @@ export async function getStaticProps(context) {
   const eventId = context.params.eventId;
   const selectedEvent = await getEventById(eventId);
 
-  console.log("Inuti getStaticProps");
-
-  if (!eventId) {
-    return "0";
+  if (!selectedEvent) {
+    return { notFound: true };
   }
 
   return {
-    props: { selectedEvent: selectedEvent },
+    props: {
+      selectedEvent: selectedEvent,
+    },
+    revalidate: 30,
+    notFound: Boolean(!selectedEvent ? true : false),
   };
 }
 
